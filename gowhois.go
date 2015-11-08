ec9ea85e9dc92629b46907b9c70776df182bd0fc
aaron.hackney <aaron.hackney@rackspace.com>
2015-11-08 12:30:06 -0600

    First Commit

---
+++ gowhois.go
@@ -0,0 +1,121 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+)
+
+func getContent(url string) ([]byte, error) {
+
+	req, err := http.NewRequest("GET", url, nil)
+
+	req.Header.Set("User-Agent", "Golang WHOIS Aaron 1.0")
+	req.Header.Set("Accept", "application/json")
+
+	if err != nil {
+		return nil, err
+	}
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+
+	if err != nil {
+		return nil, err
+	}
+
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+
+	if err != nil {
+		return nil, err
+	}
+	
+	return body, nil
+}
+
+func getWhois(url string) (*WhoisRecord, error) {
+	content, err := getContent(fmt.Sprintf(url))
+
+	var whoisRecord WhoisRecord
+		
+	err = json.Unmarshal(content, &whoisRecord)
+	if err != nil {
+		// An error occurred while converting our JSON to an object
+		fmt.Println("func getWhois(url string) (*WhoisRecord, error)")
+		fmt.Println(err)
+		return nil, err
+	}
+	
+	return &whoisRecord, err
+}
+
+func getCustRecord(url string) (*CustomerRecord, error) {
+	content, err := getContent(fmt.Sprintf(url))
+	
+	var customerRecord CustomerRecord
+	
+	err = json.Unmarshal(content, &customerRecord)
+
+	if err != nil {
+		// An error occurred while converting our JSON to an object
+		fmt.Println("func getCustRecord(url string) (*CustomerRecord, error)")
+		fmt.Println(err)
+		return nil, err
+	}
+	
+	return &customerRecord, err
+}
+
+func getOrgRecord(url string) (*OrgRecord, error) {
+	content, err := getContent(fmt.Sprintf(url))
+	
+	var orgRecord OrgRecord
+	
+	err = json.Unmarshal(content, &orgRecord)
+
+	if err != nil {
+		// An error occurred while converting our JSON to an object
+		fmt.Println("func getOrgRecord(url string) (*OrgRecord, error)")
+		fmt.Println(err)
+		return nil, err
+	}
+	
+	return &orgRecord, err
+}
+
+func main() {
+
+	//TODO: CLI input validation
+	var customerRecord *CustomerRecord
+	var orgRecord *OrgRecord
+	
+	argsWithoutProg := os.Args[1:]
+	url := "http://whois.arin.net/rest/ip/" + argsWithoutProg[0]
+	whois, _ := getWhois(url)
+	
+	if string(whois.Net.OwnerInfo.Reference) != "" {
+		customerRecord, _ = getCustRecord(string (whois.Net.OwnerInfo.Reference))		
+	}
+
+	if string(whois.Net.OrgRef.Reference) != "" {
+		orgRecord, _ = getOrgRecord(string (whois.Net.OrgRef.Reference))		
+	}
+
+	var returnJson ReturnJSON
+	returnJson.WhoisRecord = whois
+	returnJson.CustomerRecord = customerRecord
+	returnJson.OrgRecord = orgRecord
+	
+	
+	jsonOutput, err := json.MarshalIndent(&returnJson, "", "\t")
+
+	if err != nil {
+		fmt.Println("err:", err.Error())
+	}
+
+	fmt.Printf("%v", string(jsonOutput))
+}
---
+++ structs.go
@@ -0,0 +1,135 @@
+package main
+
+	type Handle struct {
+			Handle string `json:"$"`
+		}
+		
+	type Name struct {
+			Name string `json:"$"`
+		}
+	
+	type StreetAddress struct {
+			Line struct {
+				StreetAddress string `json:"$"`
+			}
+		}
+	
+	type City struct {
+			Name string `json:"$"`
+		}
+		
+	type State struct {
+			State string `json:"$"`
+		} // `json:"iso3166-2"`
+			
+	type PostalCode struct {
+			PostalCode string `json:"$"`
+		}
+	
+	type Country struct {
+			Name struct {
+				Name string `json:"$"`
+			}
+			Code2 struct {
+				Code2 string `json:"$"`
+			}	
+		} // `json:"iso3166-1"`
+		
+type CustomerRecord struct {
+	Customer struct {
+		Handle Handle
+		Name Name
+		StreetAddress StreetAddress
+		City City
+		State State `json:"iso3166-2"`
+		PostalCode PostalCode
+		Country Country `json:"iso3166-1"`
+	}
+}
+
+type OrgRecord struct {
+	Org struct {
+		Handle Handle
+		Name Name
+		StreetAddress StreetAddress
+		City City
+		State State `json:"iso3166-2"`
+		PostalCode PostalCode
+		Country Country `json:"iso3166-1"`	
+	}
+}
+
+type WhoisRecord struct {
+	Net struct {
+		NetworkRef struct {
+			NetworkRef string `json:"$"`
+		} `json:"ref"`
+		
+		ParentNetworkRef struct {
+			Reference string `json:"$"`
+			Handle    string `json:"@handle"`
+			Name      string `json:"@name"`
+		} `json:"parentNetRef"`
+
+		EndAddress struct {
+			EndAddress string `json:"$"`
+		}
+
+		StartAddress struct {
+			StartAddress string `json:"$"`
+		}
+
+		OwnerInfo struct {
+			Name          string `json:"@name"`
+			Handle        string `json:"@handle"`
+			Reference 	  string `json:"$"`
+		} `json:"customerRef"`
+
+		OrgRef struct {
+			Name      string `json:"@name"`
+			Handle    string `json:"@handle"`
+			Reference string `json:"$"`
+		} `json:"orgRef"`
+
+		Version struct {
+			Version string `json:"$"`
+		}
+		UpdateDate struct {
+			UpdateDate string `json:"$"`
+		}
+		Name struct {
+			Name string `json:"$"`
+		}
+
+		Handle struct {
+			Handle string `json:"$"`
+		}
+		NetBlocks        struct {
+			Netblock struct {
+				CidrLength struct {
+					CidrLength string `json:"$"`
+				}
+				Description struct {
+					Description string `json:"$"`
+				}
+				EndAddress struct {
+					EndAddress string `json:"$"`
+				}
+
+				StartAddress struct {
+					StartAddress string `json:"$"`
+				}
+
+				BlockType struct {
+					Type string `json:"$"`
+				} `json:"type"`
+			}
+		}
+	}
+}
+
+type ReturnJSON struct {
+	WhoisRecord *WhoisRecord `json:"whoIsRecord"`
+	CustomerRecord *CustomerRecord `json:"customerRecord,omitempty"`
+	OrgRecord *OrgRecord `json:"orgRecord,omitempty"`
+}
